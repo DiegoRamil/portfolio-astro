76e11edfbd83f83e2006fef5205e0b6a76b36aea
DiegoRamil <nxquumodding@gmail.com>
2024-12-24 11:51:48 +0100

    style(ui): add more gap

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,24 +3,22 @@ import TerminalLine from "@/components/terminal-line";
 
 const Projects = () => {
   return (
-    <section className="mb-4">
-      <TerminalLine prompt="ls -l projects/" className="">
-        {projects.map((project, index) => (
-          <div key={index} className="mb-2">
-            <span className="text-yellow-500">{project.name}</span> -{" "}
-            {project.description}
-            <a
-              href={project.url}
-              target="_blank"
-              rel="noopener"
-              className="ml-4 text-indigo-500 border-indigo-500 border-b-2"
-            >
-              View
-            </a>
-          </div>
-        ))}
-      </TerminalLine>
-    </section>
+    <TerminalLine prompt="ls -l projects/" className="">
+      {projects.map((project, index) => (
+        <div key={index} className="mb-2">
+          <span className="text-yellow-500">{project.name}</span> -{" "}
+          {project.description}
+          <a
+            href={project.url}
+            target="_blank"
+            rel="noopener"
+            className="ml-4 text-indigo-500 border-indigo-500 border-b-2"
+          >
+            View
+          </a>
+        </div>
+      ))}
+    </TerminalLine>
   );
 };
 
